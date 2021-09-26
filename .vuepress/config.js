e1aadd5816a1c1735e5abf41a9e3d37f9eb8aa83
yasha <596484539@qq.com>
2021-09-26 14:32:45 +0800

    例行提交

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,86 +1,82 @@
 module.exports = {
-  "title": "RealHeng",
-  "description": "RealHeng's blog",
-  "dest": "public",
-  "head": [
+  title: 'RealHeng',
+  description: "RealHeng's blog",
+  dest: 'public',
+  head: [
     [
-      "link",
+      'link',
       {
-        "rel": "icon",
-        "href": "/favicon.ico"
+        rel: 'icon',
+        href: '/favicon.ico'
       }
     ],
     [
-      "meta",
+      'meta',
       {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no'
       }
     ]
   ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
+  theme: 'reco',
+  themeConfig: {
+    nav: [
       {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
+        text: 'Home',
+        link: '/',
+        icon: 'reco-home'
       },
       {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
+        text: 'TimeLine',
+        link: '/timeline/',
+        icon: 'reco-date'
       },
       {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
+        text: 'Docs',
+        icon: 'reco-message',
+        items: [
           {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
+            text: 'vuepress-reco',
+            link: '/docs/theme-reco/'
           }
         ]
       },
       {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
+        text: 'Contact',
+        icon: 'reco-message',
+        items: [
           {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
+            text: 'GitHub',
+            link: 'https://github.com/recoluan',
+            icon: 'reco-github'
           }
         ]
       }
     ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
+    sidebar: {
+      '/docs/theme-reco/': ['', 'theme', 'plugin', 'api']
     },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
+    type: 'blog',
+    blogConfig: {
+      category: {
+        location: 2,
+        text: 'Category'
       },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
+      tag: {
+        location: 3,
+        text: 'Tag'
       }
     },
-    "logo": "https://gitee.com/leoyzh/imgbed/raw/master/img/20210926104301.jpg",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "RealHeng",
-    "authorAvatar": "https://gitee.com/leoyzh/imgbed/raw/master/img/20210926104301.jpg",
-    "startYear": "2021",
+    logo: 'https://gitee.com/leoyzh/imgbed/raw/master/img/20210926104301.jpg',
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: 'Last Updated',
+    author: 'RealHeng',
+    authorAvatar:
+      'https://gitee.com/leoyzh/imgbed/raw/master/img/20210926104301.jpg',
+    startYear: '2021'
   },
-  "markdown": {
-    "lineNumbers": true
+  markdown: {
+    lineNumbers: true
   }
 }
